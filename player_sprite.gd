31726d602df9af580872383980e27421983497ff
Schön <mschoen@united-security-providers.ch>
2024-09-01 20:06:18 +0200

    Updated on Windows

---
+++ player_sprite.gd
@@ -0,0 +1,24 @@
+extends Sprite2D
+
+var speed = 400
+var angular_speed = PI
+signal health_changed(old_value, new_value)
+
+func _init():
+	print("Hello, world!")
+
+func _process(delta):
+	rotation += angular_speed * delta
+	var velocity = Vector2.UP.rotated(rotation) * speed
+	position += velocity * delta
+
+
+func _on_button_pressed():
+	set_process(not is_processing())
+	
+func _ready():
+	var timer = get_node("Timer")
+	timer.timeout.connect(_on_timer_timeout)
+	
+func _on_timer_timeout():
+	visible = not visible
